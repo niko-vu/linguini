2f5b07d6cb73f3e60e9213b0404dcadaee127331
Natecon2 <frankenfieldnathan5@gmail.com>
2023-09-25 18:55:31 -0700

    Removed and replaced some of previous code. Added translate file

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Product = require('./Product');
+const Product = require('./translate');
 const Category = require('./Category');
-const Order = require('./Order');
+const Order = require('./language');
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, translate, Category, language };
