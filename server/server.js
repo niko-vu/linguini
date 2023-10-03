b9d4e77c5a418b6b3639db82870e832157d9314b
fearoffun <480dunce@gmail.com>
2023-10-02 21:21:15 -0700

    PLEASE WORK

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI);
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGO_URI);
 
 const { authMiddleware } = require('./utils/auth');
 
