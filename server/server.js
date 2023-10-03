1b1cb93d52936d2466948298f6013ac89088064e
fearoffun <480dunce@gmail.com>
2023-10-02 21:05:40 -0700

    mongoose addition

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, options);
+
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
