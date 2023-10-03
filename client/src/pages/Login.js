ba707b5d118daad3b657a5e9a01b01c554677464
fearoffun <480dunce@gmail.com>
2023-10-02 17:48:01 -0700

    added login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -43,8 +43,10 @@ const Login = (props) => {
   return (
     <main className="flex-row justify-center mb-4">
       <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+        <div className="card"
+          style={{ borderRadius: '15px' }}>
+          <h4 className="card-header bg-dark text-light p-2"
+            style={{ borderRadius: '13px' }}>Login</h4>
           <div className="card-body">
             {data ? (
               <p>
